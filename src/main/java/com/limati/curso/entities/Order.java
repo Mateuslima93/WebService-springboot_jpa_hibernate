5ce9456882a5ccca676d8dcb58bdd6388246a3bb
Mateuslima93 <mateus_limaarodrigues@hotmail.com>
2020-07-21 18:35:33 -0300

    Subtotal e Total methods

--- src/main/java/com/limati/curso/entities/Order.java
+++ src/main/java/com/limati/curso/entities/Order.java
@@ -84,7 +84,14 @@ public class Order implements Serializable {
 	public void setClient(User client) {
 		this.client = client;
 	}
-
+	
+	public Double getTotal() {
+		Double sum = 0.0;
+		for (OrderItem order: items) {
+			sum += order.getSubTotal();
+		}
+		return sum;
+	}
 	@Override
 	public int hashCode() {
 		final int prime = 31;
--- src/main/java/com/limati/curso/repositories/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.limati.curso.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.limati.curso.entities.Payment;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-
-}
