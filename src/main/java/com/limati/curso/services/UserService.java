be0acf39db1f529dc555385406036352d871b6dc
Mateuslima93 <mateus_limaarodrigues@hotmail.com>
2020-07-23 12:23:42 -0300

    Exception Handler findById

--- src/main/java/com/limati/curso/services/UserService.java
+++ src/main/java/com/limati/curso/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.limati.curso.entities.User;
 import com.limati.curso.repositories.UserRepository;
+import com.limati.curso.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -20,7 +21,7 @@ public class UserService {
 	}
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 		//get retorna objeto user da classe optional.
 	}
 	public User insert(User obj) {
